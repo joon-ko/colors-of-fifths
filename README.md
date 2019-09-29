62532de241a42a5859405c5c0d54b3bf23f718bc
Joonho Ko <joonhok@mit.edu>
2019-09-28 23:16:16 -0400

    version 1

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# colors-of-fifths
-exploring the intersection of the color wheel and the circle of fifths
+# overview
+**colors of fifths** is an intersection of the (RGB) [color wheel](https://en.wikipedia.org/wiki/Color_wheel) with the [circle of fifths](https://en.wikipedia.org/wiki/Circle_of_fifths). namely, you can traverse the color wheel clockwise by playing the circle of fifths, and you can traverse the wheel counterclockwise by playing the circle of fourths.
+
+you can use your computer keyboard as a piano:
+
+|computer key|piano key|
+|---|---|
+|A|C|
+|W|C#/D♭|
+|S|D|
+|E|D#/E♭|
+|F|F|
+|T|F#/G♭|
+|G|G|
+|Y|G#/A♭|
+|H|A|
+|U|A#/B♭|
+|J|B|
+
+the motivational sketch behind the idea is included as `sketch.png`. there are some interesting relationships between colors and chords that i'll probably use this project to explore a bit more!
---
+++ main.js
@@ -0,0 +1,83 @@
+let keyToNote = new Map();
+keyToNote.set('KeyA', 'C');
+keyToNote.set('KeyW', 'Db');
+keyToNote.set('KeyS', 'D');
+keyToNote.set('KeyE', 'Eb');
+keyToNote.set('KeyD', 'E');
+keyToNote.set('KeyF', 'F');
+keyToNote.set('KeyT', 'Gb');
+keyToNote.set('KeyG', 'G');
+keyToNote.set('KeyY', 'Ab');
+keyToNote.set('KeyH', 'A');
+keyToNote.set('KeyU', 'Bb');
+keyToNote.set('KeyJ', 'B');
+
+let sounds = new Map();
+sounds.set('C', 'notes/C.mp3');
+sounds.set('Db', 'notes/Db.mp3');
+sounds.set('D', 'notes/D.mp3');
+sounds.set('Eb', 'notes/Eb.mp3');
+sounds.set('E', 'notes/E.mp3');
+sounds.set('F', 'notes/F.mp3');
+sounds.set('Gb', 'notes/Gb.mp3');
+sounds.set('G', 'notes/G.mp3');
+sounds.set('Ab', 'notes/Ab.mp3');
+sounds.set('A', 'notes/A.mp3');
+sounds.set('Bb', 'notes/Bb.mp3');
+sounds.set('B', 'notes/B.mp3');
+
+document.addEventListener('keydown', e => {
+	play(keyToNote.get(e.code));
+})
+
+function play(note) {
+	let noteDiv = document.createElement('div');
+	noteDiv.classList.add(note);
+
+	switch (note) {
+		case 'C':
+		case 'Gb':
+			noteDiv.classList.add('note-y');
+			noteDiv.style.left = `${randInt(0, window.innerWidth - 200)}px`;
+			break;
+		case 'Eb':
+		case 'A':
+			noteDiv.classList.add('note-x');
+			noteDiv.style.bottom = `${randInt(0, window.innerHeight - 200)}px`;
+			break;
+		case 'Db':
+		case 'F':
+			noteDiv.classList.add('note-y');
+			noteDiv.style.left = `${randInt(-.57*window.innerHeight, window.innerWidth)}px`;
+			break;
+		case 'G':
+		case 'B':
+			noteDiv.classList.add('note-y');
+			noteDiv.style.left = `${randInt(0, window.innerWidth + .57*window.innerHeight)}px`;
+			break;
+		case 'Ab':
+		case 'Bb':
+			noteDiv.classList.add('note-x');
+			noteDiv.style.bottom = `${randInt(-.57*window.innerWidth, window.innerHeight)}px`;
+			break;
+		case 'D':
+		case 'E':
+			noteDiv.classList.add('note-x');
+			noteDiv.style.bottom = `${randInt(0, window.innerHeight + .57*window.innerWidth)}px`;
+			break;
+	}
+
+	noteDiv.addEventListener('animationend', e => {
+		noteDiv.remove();
+	});
+
+	let noteAudio = new Audio(sounds.get(note));
+	noteAudio.play();
+
+	document.getElementById('content').appendChild(noteDiv);
+}
+
+// return a random integer in [start, end)
+function randInt(start, end) {
+	return start + Math.floor(Math.random() * (end - start));
+}
